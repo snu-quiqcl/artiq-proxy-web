6cab56b2ed47720c86ba07a11b9459096233ea6c
BECATRUE <jaehun0121@snu.ac.kr>
2026-04-01 13:37:48 +0900

    Resolve Pylint errors
    See also: #2

--- artiq_proxy_web/users/serializers.py
+++ artiq_proxy_web/users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
 
-class LoginSerializer(serializers.Serializer):
+class LoginSerializer(serializers.Serializer):  # pylint: disable=abstract-method
     username = serializers.CharField()
     password = serializers.CharField(trim_whitespace=False)
